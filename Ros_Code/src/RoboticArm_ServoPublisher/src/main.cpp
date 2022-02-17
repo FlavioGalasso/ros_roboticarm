f57116a23754446a903bca2ceb80acf13f741ead
redmodder <flavio.galasso2.0@gmail.com>
2022-02-17 10:07:49 -0500

    implemented constraints on angles to sanitize input

--- Ros_Code/src/RoboticArm_ServoPublisher/src/main.cpp
+++ Ros_Code/src/RoboticArm_ServoPublisher/src/main.cpp
@@ -7,6 +7,34 @@ const double Pi = 3.14159265358979323846264; //constant expression
 const double toRadian = Pi / 180;
 const double toDegree = 180 / Pi;
 
+const double q1Min = 0;
+const double q1Max = 90;
+const double q1Min2 = 270;
+const double q1Max2 = 359;
+
+const double q2Min = 20;
+const double q2Max = 160;
+
+const double q3Min = 0;
+const double q3Max = 90;
+const double q3Min2 = 315;
+const double q3Max2 = 359;
+
+const double q4Min = 0;
+const double q4Max = 100;
+
+const double h1 = 1.75;
+const double l1 = 0.3;
+const double l2 = 8.0;
+const double l3 = 8.0;
+const double le = 6.0;
+
+void constrain(double &angle, double minangle,double maxangle){
+  angle = angle > maxangle ? maxangle : angle;
+  angle = angle < minangle ? minangle : angle;
+}
+
+
 int main(int argc, char **argv)
 {
   
@@ -18,11 +46,7 @@ int main(int argc, char **argv)
   ros::Publisher servo4pub = n.advertise<std_msgs::UInt16>("servo4", 1000);
   tf::TransformBroadcaster tfbroadcaster;
   ros::Rate loop_rate(10);
-  double h1 = 1.75;
-  double l1 = 0.3;
-  double l2 = 8.0;
-  double l3 = 8.0;
-  double le = 6.0;
+
   double q1 = 0;
   double q2 = 0;
   double q3 = 0;
@@ -36,28 +60,54 @@ int main(int argc, char **argv)
     std_msgs::UInt16 msg;
     int servocmd = 0;
     std::cin >> servocmd;
-    if(servocmd < 2000 && servocmd >= 0){
-        msg.data = servocmd - 1000;
+    if(servocmd < 2000 && servocmd >= 1000){
+        q1 = servocmd - 1000;
+        
+        if(q1 >= q1Min && q1 < q1Min2)
+          constrain(q1,q1Min,q1Max);
+        else if (q1 >= q1Min2)
+          constrain(q1,q1Min2,q1Max2);
+
+        msg.data = q1;
         servo1pub.publish(msg);
-        q1 = msg.data * toRadian;
         ROS_INFO("%d sent to servo 1", msg.data);
+
+        q1 = q1 * toRadian;
     }
     if(servocmd < 3000 && servocmd >= 2000){
-        msg.data = servocmd - 2000;
+        q2 = servocmd - 2000;
+
+        constrain(q2,q2Min,q2Max);
+
+        msg.data = q2;
         servo2pub.publish(msg);
-        q2 = msg.data * toRadian;
         ROS_INFO("%d sent to servo 2", msg.data);
+
+        q2 = q2* toRadian;
+
     }
     if(servocmd < 4000 && servocmd >= 3000){
-        msg.data = servocmd - 3000;
+        q3 = servocmd - 3000;
+        
+        if(q3 >= q3Min && q3 < q3Min2)
+          constrain(q3,q3Min,q3Max);
+        else if (q3 >= q3Min2)
+          constrain(q3,q3Min2,q3Max2);
+        
+        msg.data=q3;
         servo3pub.publish(msg);
-        q3 = msg.data * toRadian;
         ROS_INFO("%d sent to servo 3", msg.data);
+    
+        q3 = q3 * toRadian;
+
     }
         if(servocmd < 5000 && servocmd >= 4000){
-        msg.data = servocmd - 4000;
+        q4= servocmd - 4000;
+
+        constrain(q4,q4Min,q4Max);
+
+        msg.data = q4;
         servo4pub.publish(msg);
-        q4 = msg.data;
         ROS_INFO("%d sent to servo 4", msg.data);
     }
 
